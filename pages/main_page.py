10168b6c8202828be41f662a8f85812f1d1b7b1b
Aleksey Sazonov <sazonov1988@yandex.ru>
2023-07-22 15:24:41 +0500

    Добавил тесты

---
+++ pages/main_page.py
@@ -0,0 +1,14 @@
+def test_buy_item():
+    assert 1 == 2
+
+
+def test_clear_basket():
+    assert 1 == 1
+
+
+def test_remove_item():
+    assert 1 == 2
+
+
+def test_add_item():
+    assert 1 == 1
